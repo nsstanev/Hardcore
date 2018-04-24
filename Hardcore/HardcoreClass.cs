7481eb66b48d13888a565e668630a00ee3eec8c6
Nikolay Stanev <N.stanev@synergysportstech.com>
2018-04-24 10:35:57 +0300

    Init

---
+++ Hardcore/HardcoreClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hardcore
+{
+    public class HardcoreClass
+    {
+        public void DoSomething()
+        {
+
+        }
+    }
+}
